d8940baf9d4a9a72716d5271a47174484accb8f0
Maria Senkiv <marichka.spin@gmail.com>
2020-09-23 14:46:10 +0300

    Added additional properties to Observation dto

--- src/main/java/com/proptechos/model/Observation.java
+++ src/main/java/com/proptechos/model/Observation.java
@@ -1,11 +1,14 @@
 package com.proptechos.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.proptechos.util.InstantDeserializer;
 import com.proptechos.util.InstantSerializer;
 import java.time.Instant;
+import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Observation {
 
   @JsonSerialize(using = InstantSerializer.class)
@@ -13,6 +16,10 @@ public class Observation {
   private Instant observationTime;
 
   private Double value;
+
+  private String quantityKind;
+
+  private UUID sensorId;
 
   public Instant getObservationTime() {
     return observationTime;
@@ -28,5 +35,21 @@ public class Observation {
 
   public void setValue(Double value) {
     this.value = value;
+  }
+
+  public String getQuantityKind() {
+    return quantityKind;
+  }
+
+  public void setQuantityKind(String quantityKind) {
+    this.quantityKind = quantityKind;
+  }
+
+  public UUID getSensorId() {
+    return sensorId;
+  }
+
+  public void setSensorId(UUID sensorId) {
+    this.sensorId = sensorId;
   }
 }
