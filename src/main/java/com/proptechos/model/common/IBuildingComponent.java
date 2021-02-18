f42002fbfaaec1d8911adaedefcfe1f87c7fcf47
Maria Senkiv <marichka.spin@gmail.com>
2021-02-18 14:34:49 +0200

    Added all building component classses

--- src/main/java/com/proptechos/model/common/IBuildingComponent.java
+++ src/main/java/com/proptechos/model/common/IBuildingComponent.java
@@ -3,10 +3,19 @@ package com.proptechos.model.common;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.proptechos.model.buildingcomponent.Balcony;
 import com.proptechos.model.buildingcomponent.BuildingComponent;
+import com.proptechos.model.buildingcomponent.Facade;
+import com.proptechos.model.buildingcomponent.Floor;
+import com.proptechos.model.buildingcomponent.RoofInner;
+import com.proptechos.model.buildingcomponent.RoofOuter;
 import com.proptechos.model.buildingcomponent.Room;
+import com.proptechos.model.buildingcomponent.Slab;
 import com.proptechos.model.buildingcomponent.Storey;
+import com.proptechos.model.buildingcomponent.Terrace;
 import com.proptechos.model.buildingcomponent.VirtualBuildingComponent;
+import com.proptechos.model.buildingcomponent.Wall;
+import com.proptechos.model.buildingcomponent.Zone;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +25,15 @@ import java.util.UUID;
     visible = true)
 @JsonSubTypes({
     @Type(value = BuildingComponent.class, name = "BuildingComponent"),
+    @Type(value = Balcony.class, name = "Balcony"),
+    @Type(value = Facade.class, name = "Facade"),
+    @Type(value = Floor.class, name = "Floor"),
+    @Type(value = RoofInner.class, name = "RoofInner"),
+    @Type(value = RoofOuter.class, name = "RoofOuter"),
+    @Type(value = Slab.class, name = "Slab"),
+    @Type(value = Terrace.class, name = "Terrace"),
+    @Type(value = Wall.class, name = "Wall"),
+    @Type(value = Zone.class, name = "Zone"),
     @Type(value = Room.class, name = "Room"),
     @Type(value = Storey.class, name = "Storey"),
     @Type(value = VirtualBuildingComponent.class, name = "VirtualBuildingComponent")})
