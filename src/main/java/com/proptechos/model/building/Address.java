5924ab63539911ccfefd3a53d914380053780520
Oleksandra Podhurska <oleksandra@idunrealestate.com>
2022-12-21 11:41:53 +0200

    PLAT-3602 Address improvements

--- src/main/java/com/proptechos/model/building/Address.java
+++ src/main/java/com/proptechos/model/building/Address.java
@@ -1,27 +1,81 @@
 package com.proptechos.model.building;
 
-public class Address {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.proptechos.model.Alias;
+import com.proptechos.model.TwinStatus;
+import com.proptechos.model.common.IForeignSystemObject;
+import com.proptechos.model.common.IRecClass;
+import com.proptechos.util.InstantDeserializer;
+import com.proptechos.util.InstantSerializer;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class Address implements IRecClass, IForeignSystemObject {
+
+    @JsonProperty("id")
+    private UUID id;
+
+    @JsonProperty("class")
+    private String recClass;
+
+    @JsonProperty("status")
+    private TwinStatus twinStatus;
+
+    @JsonProperty("createdTime")
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    private Instant created;
+
+    @JsonProperty("updatedTime")
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    private Instant updated;
+
+    @JsonProperty(value = "createdByAgent")
+    private String createdByAgent;
+
+    @JsonProperty(value = "updatedByAgent")
+    private String updatedByAgent;
+
+    @JsonProperty("hasAlias")
+    private List<Alias> hasAliases;
+
+    @JsonProperty("comment")
+    private HashMap<String, String> comment;
+
+    @JsonProperty("source")
+    protected HashMap<String, String> source;
+
+    @JsonProperty("littera")
+    private String littera;
+
+    @JsonProperty("popularName")
+    private String popularName;
+
+    @JsonProperty("type")
     private String type;
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
+    @JsonProperty("streetAddress")
     private String streetAddress;
 
+    @JsonProperty("postalCode")
     private String postalCode;
 
+    @JsonProperty("city")
     private String city;
 
+    @JsonProperty("countryName")
     private String countryName;
 
+    @JsonProperty("locality")
     private String locality;
 
+    @JsonProperty("region")
     private String region;
 
     public String getStreetAddress() {
@@ -70,5 +124,131 @@ public class Address {
 
     public void setRegion(String region) {
         this.region = region;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getRecClass() {
+        return recClass;
+    }
+
+    @Override
+    public void setRecClass(String recClass) {
+        this.recClass = recClass;
+    }
+
+    @Override
+    public TwinStatus getTwinStatus() {
+        return twinStatus;
+    }
+
+    @Override
+    public void setTwinStatus(TwinStatus twinStatus) {
+        this.twinStatus = twinStatus;
+    }
+
+    @Override
+    public Instant getCreated() {
+        return created;
+    }
+
+    @Override
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    @Override
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    @Override
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String getCreatedByAgent() {
+        return createdByAgent;
+    }
+
+    @Override
+    public void setCreatedByAgent(String createdByAgent) {
+        this.createdByAgent = createdByAgent;
+    }
+
+    @Override
+    public String getUpdatedByAgent() {
+        return updatedByAgent;
+    }
+
+    @Override
+    public void setUpdatedByAgent(String updatedByAgent) {
+        this.updatedByAgent = updatedByAgent;
+    }
+
+    public List<Alias> getHasAliases() {
+        return hasAliases;
+    }
+
+    public void setHasAliases(List<Alias> hasAliases) {
+        this.hasAliases = hasAliases;
+    }
+
+    @Override
+    public HashMap<String, String> getComment() {
+        return comment;
+    }
+
+    @Override
+    public void setComment(HashMap<String, String> comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String getLittera() {
+        return littera;
+    }
+
+    @Override
+    public void setLittera(String littera) {
+        this.littera = littera;
+    }
+
+    @Override
+    public String getPopularName() {
+        return popularName;
+    }
+
+    @Override
+    public void setPopularName(String popularName) {
+        this.popularName = popularName;
+    }
+
+    @Override
+    public HashMap<String, String> getSource() {
+        return source;
+    }
+
+    @Override
+    public void setSource(HashMap<String, String> source) {
+        this.source = source;
     }
 }
