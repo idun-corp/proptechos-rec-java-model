95c4ddec8137af8cf6f3340cf0031f1b46843de3
Oleksandra Podhurska <oleksandra@idunrealestate.com>
2022-12-21 13:06:56 +0200

    PLAT-3602 Address improvements

--- src/main/java/com/proptechos/model/building/Address.java
+++ src/main/java/com/proptechos/model/building/Address.java
@@ -1,10 +1,119 @@
 package com.proptechos.model.building;
 
-public class Address {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.proptechos.model.Alias;
+import com.proptechos.model.TwinStatus;
+import com.proptechos.model.common.IForeignSystemObject;
+import com.proptechos.model.common.IRecClass;
+import com.proptechos.util.InstantDeserializer;
+import com.proptechos.util.InstantSerializer;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class Address implements IRecClass, IForeignSystemObject {
+
+    @JsonProperty("id")
+    private UUID id;
+
+    @JsonProperty("class")
+    private String recClass;
+
+    @JsonProperty("status")
+    private TwinStatus twinStatus;
+
+    @JsonProperty("createdTime")
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    private Instant created;
+
+    @JsonProperty("updatedTime")
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    private Instant updated;
+
+    @JsonProperty(value = "createdByAgent")
+    private String createdByAgent;
+
+    @JsonProperty(value = "updatedByAgent")
+    private String updatedByAgent;
+
+    @JsonProperty("hasAlias")
+    private List<Alias> hasAliases;
+
+    @JsonProperty("comment")
+    private HashMap<String, String> comment;
+
+    @JsonProperty("source")
+    protected HashMap<String, String> source;
+
+    @JsonProperty("littera")
+    private String littera;
+
+    @JsonProperty("popularName")
+    private String popularName;
+
+    @JsonProperty("type")
     private String type;
 
-    private Details address;
+    @JsonProperty("streetAddress")
+    private String streetAddress;
+
+    @JsonProperty("postalCode")
+    private String postalCode;
+
+    @JsonProperty("countryName")
+    private String countryName;
+
+    @JsonProperty("locality")
+    private String locality;
+
+    @JsonProperty("region")
+    private String region;
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
     public String getType() {
         return type;
@@ -14,74 +123,121 @@ public class Address {
         this.type = type;
     }
 
-    public Details getAddress() {
-        return address;
+    @Override
+    public UUID getId() {
+        return id;
     }
 
-    public void setAddress(Details address) {
-        this.address = address;
+    @Override
+    public void setId(UUID id) {
+        this.id = id;
     }
 
-    public static class Details {
+    @Override
+    public String getRecClass() {
+        return recClass;
+    }
 
-        private String streetAddress;
+    @Override
+    public void setRecClass(String recClass) {
+        this.recClass = recClass;
+    }
 
-        private String postalCode;
+    @Override
+    public TwinStatus getTwinStatus() {
+        return twinStatus;
+    }
 
-        private String city;
+    @Override
+    public void setTwinStatus(TwinStatus twinStatus) {
+        this.twinStatus = twinStatus;
+    }
 
-        private String countryName;
+    @Override
+    public Instant getCreated() {
+        return created;
+    }
 
-        private String locality;
+    @Override
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
 
-        private String region;
+    @Override
+    public Instant getUpdated() {
+        return updated;
+    }
 
-        public String getStreetAddress() {
-            return streetAddress;
-        }
+    @Override
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
 
-        public void setStreetAddress(String streetAddress) {
-            this.streetAddress = streetAddress;
-        }
+    @Override
+    public String getCreatedByAgent() {
+        return createdByAgent;
+    }
 
-        public String getPostalCode() {
-            return postalCode;
-        }
+    @Override
+    public void setCreatedByAgent(String createdByAgent) {
+        this.createdByAgent = createdByAgent;
+    }
 
-        public void setPostalCode(String postalCode) {
-            this.postalCode = postalCode;
-        }
+    @Override
+    public String getUpdatedByAgent() {
+        return updatedByAgent;
+    }
 
-        public String getCity() {
-            return city;
-        }
+    @Override
+    public void setUpdatedByAgent(String updatedByAgent) {
+        this.updatedByAgent = updatedByAgent;
+    }
 
-        public void setCity(String city) {
-            this.city = city;
-        }
+    public List<Alias> getHasAliases() {
+        return hasAliases;
+    }
 
-        public String getCountryName() {
-            return countryName;
-        }
+    public void setHasAliases(List<Alias> hasAliases) {
+        this.hasAliases = hasAliases;
+    }
 
-        public void setCountryName(String countryName) {
-            this.countryName = countryName;
-        }
+    @Override
+    public HashMap<String, String> getComment() {
+        return comment;
+    }
 
-        public String getLocality() {
-            return locality;
-        }
+    @Override
+    public void setComment(HashMap<String, String> comment) {
+        this.comment = comment;
+    }
 
-        public void setLocality(String locality) {
-            this.locality = locality;
-        }
+    @Override
+    public String getLittera() {
+        return littera;
+    }
 
-        public String getRegion() {
-            return region;
-        }
+    @Override
+    public void setLittera(String littera) {
+        this.littera = littera;
+    }
 
-        public void setRegion(String region) {
-            this.region = region;
-        }
+    @Override
+    public String getPopularName() {
+        return popularName;
+    }
+
+    @Override
+    public void setPopularName(String popularName) {
+        this.popularName = popularName;
+    }
+
+    @Override
+    public HashMap<String, String> getSource() {
+        return source;
+    }
+
+    @Override
+    public void setSource(HashMap<String, String> source) {
+        this.source = source;
     }
 }
